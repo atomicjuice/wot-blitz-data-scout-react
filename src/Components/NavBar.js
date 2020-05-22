5e3182f9fed76857da1da7ce1b5e09b77f856659
atomicjuice <keemowatson1@gmail.com>
2020-05-22 20:27:09 +0100

    started work on rearranging the navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,15 +8,16 @@ const NavBar = () => {
 
     // </nav>
     <div>
-      <nav className='header'>
+      {/* <nav className='header'>
         World of Tanks Blitz Data Scout
-    </nav>
+    </nav> */}
       <nav className='navigate'>
         <ul>
           <li className='b'><Link to='/usersearch'>Player Search</Link></li>
-          <Link to='/clansearch'>Clan Search</Link>
-          <Link to='/playerlist'>Player List</Link>
-          <Link to='/clanlist'>Clan List</Link>
+          <li><Link to='/clansearch'>Clan Search</Link></li>
+          <li><Link to='/playerlist'>Player List</Link></li>
+          <li><Link to='/clanlist'>Clan List</Link></li>
+          <li className='appName'>World of Tanks Blitz Data Scout</li>
         </ul>
       </nav>
     </div>
