bdd9b40082ddd0c36d086dbddc8792f3d27d3260
atomicjuice <keemowatson1@gmail.com>
2020-05-23 21:22:58 +0100

    more changes to the navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,16 +8,14 @@ const NavBar = () => {
 
     // </nav>
     <div>
-      {/* <nav className='header'>
-        World of Tanks Blitz Data Scout
-    </nav> */}
       <nav className='navigate'>
+        
         <ul>
-          <li className='b'><Link to='/usersearch'>Player Search</Link></li>
-          <li><Link to='/clansearch'>Clan Search</Link></li>
-          <li><Link to='/playerlist'>Player List</Link></li>
-          <li><Link to='/clanlist'>Clan List</Link></li>
-          <li className='appName'>World of Tanks Blitz Data Scout</li>
+          <li className='appName'> World of Tanks Blitz Data Scout</li>
+          <li className='linkStyle'><Link to='/usersearch'>Player Search</Link></li>
+          <li className='linkStyle'><Link to='/clansearch'>Clan Search</Link></li>
+          <li className='linkStyle'><Link to='/playerlist'>Player List</Link></li>
+          <li className='linkStyle'><Link to='/clanlist'>Clan List</Link></li>
         </ul>
       </nav>
     </div>
