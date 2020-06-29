1bae79a38a77285b062e8d2db80d809f7e136c84
atomicjuice <keemowatson1@gmail.com>
2020-06-29 06:15:56 +0100

    added typewriter effect to the himescreen title

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -4,25 +4,14 @@ import '../Css/HomeScreen.css'
 class HomeScreen extends Component {
 
 
-    typeWriter = () => {
-        var i = 0;
-        var txt = 'World of Tanks Data Scout'; /* The text */
-        var speed = 50; /* The speed/duration of the effect in milliseconds */
-        if (i < txt.length) {
-            document.getElementsByClassName("title").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(this.typeWriter, speed);
-            // requestAnimationFrame(this.typeWriter)
-        }
-    }
+
 
 
 
     render() {
         return (
             <div>
-                <p className='title'>World of Tanks Data Scout</p>
-                {/* {this.typeWriter()} */}
+                <p className='title'>World of Tanks Blitz Data Scout</p>
             </div>
         );
     }
