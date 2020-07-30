217b31376c60de4b52a35808b7e7ef459e20223f
atomicjuice <keemowatson1@gmail.com>
2020-07-31 00:13:10 +0100

    added context to the clan search page

--- src/Components/ClanSearch.js
+++ src/Components/ClanSearch.js
@@ -33,10 +33,10 @@ class ClanSearch extends Component {
     fetch(`https://api.wotblitz.eu/wotb/clans/list/?application_id=${this.props.apikey}&search=${name}`)
       .then(resp => resp.json())
       .then(json => {
-        if(json.data[0] === undefined){
+        if (json.data[0] === undefined) {
           alert('Clan Not Found')
         }
-        else{
+        else {
           const ID = json.data[0].clan_id
           this.setState({
             id: ID
@@ -49,12 +49,16 @@ class ClanSearch extends Component {
 
   render() {
     return (
-      <div className='searchBar'> 
-        <form onSubmit={this.handleSubmit}>
-          <input name="clanname" type="text" placeholder="Enter Clan Name" className='input'></input>
-          <input type="submit" value="Scout" className='button' />
-        </form>
+      <div>
+        <p id='clanSearchContext'>Type in the name of the clan you wish to scout</p>
+        <div className='searchBar'>
+          <form onSubmit={this.handleSubmit}>
+            <input name="clanname" type="text" placeholder="Enter Clan Name" className='input'></input>
+            <input type="submit" value="Scout" className='button' />
+          </form>
+        </div>
       </div>
+
     );
   }
 }
