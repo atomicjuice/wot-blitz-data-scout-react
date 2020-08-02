9fb8f7f917dc1c664625b39f85a9d6bfa5a2490a
atomicjuice <keemowatson1@gmail.com>
2020-08-02 16:54:25 +0100

    managed to make the typewriter effect show up after the background video starts

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -3,11 +3,26 @@ import '../Css/HomeScreen.css'
 
 class HomeScreen extends Component {
 
+    renderTitle = () => {
+
+       return <p id='title'>World of Tanks Blitz Data Scout</p>
+    }
+
+    componentDidMount () {
+        setTimeout(() =>{
+            const home = document.querySelector('#home')
+            let p = document.createElement('p')
+            p.id = 'title'
+            p.innerText = 'World of Tanks Blitz Data Scout'
+            home.appendChild(p)
+        }, 2000)
+
+    }
+
     render() {
         return (
-            <div>
+            <div id='home'>
                 <h1 className='context'>Welcome to..</h1>
-                <p className='title'>World of Tanks Blitz Data Scout</p>
             </div>
         );
     }
